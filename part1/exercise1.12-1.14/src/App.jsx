dad7ae713097a0444c0da765bee100beae4f65cf
vanvuvuong <vanvuvuong3@gmail.com>
2024-10-27 21:22:58 +0700

    finish exercise 1.13, 1.14

--- part1/exercise1.12-1.14/src/App.jsx
+++ part1/exercise1.12-1.14/src/App.jsx
@@ -3,6 +3,20 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+const MostVotesAnecdote = ({ countVote, anecdotes }) => {
+    let mostVote = Math.max(...countVote);
+
+    if (mostVote == 0) {
+        return <></>;
+    }
+    return (
+        <>
+            <h1>Most votes Anecdotes</h1>
+            <div>{anecdotes[countVote.indexOf(mostVote)]}</div>
+        </>
+    );
+};
+
 const App = () => {
     const anecdotes = [
         "If it hurts, do it more often.",
@@ -16,14 +30,24 @@ const App = () => {
     ];
 
     const [selected, setSelected] = useState(0);
+    const [countVote, setCount] = useState(new Array(anecdotes.length).fill(0));
     const onClick = () => {
         let newSelected = Math.floor(Math.random() * anecdotes.length);
         setSelected(newSelected);
     };
+    const vote = () => {
+        let newCountVote = [...countVote];
+        newCountVote[selected] += 1;
+        setCount(newCountVote);
+    };
+
     return (
         <>
+            <h1>Anecdotes of the day</h1>
             <div>{anecdotes[selected]}</div>
+            <button onClick={vote}>Vote</button>
             <button onClick={onClick}>Next anecdotes</button>
+            <MostVotesAnecdote countVote={countVote} anecdotes={anecdotes} />
         </>
     );
 };
