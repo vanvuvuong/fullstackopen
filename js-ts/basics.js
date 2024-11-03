54418f1a5a8a871b4a7902bce77d52e03417972c
vanvuvuong <vanvuvuong3@gmail.com>
2024-11-03 21:04:58 +0700

    update js array & if..else, switch..case

--- js-ts/basics.js
+++ js-ts/basics.js
@@ -11,11 +11,32 @@ t.push(5, 5); // add new number to arrays
 t.pop(); // get the last element & remove it from the array
 const t2 = t.concat(5); // creates new array based on t & add 5 as the last element
 const m1 = t.map((value) => value * 2); // [2, 4, 6]
+t.filter((number) => number > 0); // [1,3,5]
 t.indexOf(1); // 0
 t.slice(1, 2); // [-1]
 t.splice(-1); // [3]
 t.find(1);
 t.filter(1);
+
+// if...else
+const result = 1 > 0 ? true : false;
+if (1 > 0) {
+    console.log(true);
+} else if (1 < 0) {
+    console.log(false);
+} else {
+    console.log("not happened");
+}
+
+switch (t) {
+    case 1:
+        console.log("t=1");
+        break;
+
+    default:
+        console.log("t is nothing");
+        break;
+}
 
 // loop
 for (let i = 0; i < t.length; i++) {
